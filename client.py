64c0f400a6fb9abc8aab100dec3150778cfee7a0
Henry Samuelson <hsamuelson@users.noreply.github.com>
2019-06-04 09:45:20 -0700

    fixes

--- client.py
+++ client.py
@@ -5,9 +5,55 @@
 #
 # This is the main client for git message 
 
+
+import subprocess
 import requests
 import os
+import base64
 from bs4 import BeautifulSoup
+
+from contextlib import contextmanager
+import sys, os
+
+#Starting Messages
+#print("            o8o      .   ooo        ooooo ")                                                           
+#print("            `'    .o8   `88.       .888'           ")                                                 
+#print(" .oooooooo oooo  .o888oo  888b     d'888   .ooooo.   .oooo.o  .oooo.o  .oooo.    .oooooooo  .ooooo.  ")
+#print("888' `88b  `888    888    8 Y88. .P  888  d88' `88b d88(  '8 d88(  '8 `P  )88b  888' `88b  d88' `88b ")
+#print("888   888   888    888    8  `888'   888  888ooo888 `'Y88b.  `'Y88b.   .oP'888  888   888  888ooo888 ")
+#print("`88bod8P'   888    888 .  8    Y     888  888    .o o.  )88b o.  )88b d8(  888  `88bod8P'  888    .o ")
+#print("`8oooooo.  o888o   '888' o8o        o888o `Y8bod8P' 8""888P' 8""888P' `Y888""8o     `8oooooo.`Y8bod8P' ")
+#print("d'     YD                                                                       d'     YD            ")
+#print("'Y88888P'                                                                       'Y88888P'            ")
+os.system("cls")                       
+print("")                                                                              
+print("  ▄████  ██▓▄▄▄█████▓ ███▄ ▄███▓▓█████   ██████   ██████  ▄▄▄        ▄████ ▓█████") 
+print(" ██▒ ▀█▒▓██▒▓  ██▒ ▓▒▓██▒▀█▀ ██▒▓█   ▀ ▒██    ▒ ▒██    ▒ ▒████▄     ██▒ ▀█▒▓█   ▀ ")
+print("▒██░▄▄▄░▒██▒▒ ▓██░ ▒░▓██    ▓██░▒███   ░ ▓██▄   ░ ▓██▄   ▒██  ▀█▄  ▒██░▄▄▄░▒███   ")
+print("░▓█  ██▓░██░░ ▓██▓ ░ ▒██    ▒██ ▒▓█  ▄   ▒   ██▒  ▒   ██▒░██▄▄▄▄██ ░▓█  ██▓▒▓█  ▄ ")
+print("░▒▓███▀▒░██░  ▒██▒ ░ ▒██▒   ░██▒░▒████▒▒██████▒▒▒██████▒▒ ▓█   ▓██▒░▒▓███▀▒░▒████▒")
+print(" ░▒   ▒ ░▓    ▒ ░░   ░ ▒░   ░  ░░░ ▒░ ░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░ ▒▒   ▓▒█░ ░▒   ▒ ░░ ▒░ ░")
+print("  ░   ░  ▒ ░    ░    ░  ░      ░ ░ ░  ░░ ░▒  ░ ░░ ░▒  ░ ░  ▒   ▒▒ ░  ░   ░  ░ ░  ░")
+print("░ ░   ░  ▒ ░  ░      ░      ░      ░   ░  ░  ░  ░  ░  ░    ░   ▒   ░ ░   ░    ░   ")
+print("      ░  ░                  ░      ░  ░      ░        ░        ░  ░      ░    ░  ░")
+print("")
+print("'I don't want my name on this project' - Henry Samuelson 2019")
+print("")
+print("Connect to Sever (git repo):")
+
+
+@contextmanager
+def suppress_stdout():
+    with open(os.devnull, "w") as devnull:
+        old_stdout = sys.stdout
+        sys.stdout = devnull
+        try:  
+            yield
+        finally:
+            sys.stdout = old_stdout
+
+
+
 
 noServer = True
 client = requests.session()
@@ -24,7 +70,9 @@ while noServer:
             noServer = False
 
             #clone repo
-            command = "git clone" + ServerPath
+            #with open(os.devnull, 'wb') as devnull:
+            #    subprocess.check_call(['git clone ' + ServerPath], stdout=devnull, stderr=subprocess.STDOUT)
+            command = "git clone " + ServerPath
             os.system(command)
 
             print("Connected To Server")
@@ -34,20 +82,31 @@ while noServer:
 
 messageClient = True
 prompt = "(" + REPOPATH + ")>"
-chatPath = str(ServerPath + "chat.txt")
-
+os.chdir(REPOPATH.split("/")[1])
+url = "https://api.github.com/repos/" + REPOPATH + "/contents/chat.txt"
 
 while messageClient:
-    client.get(chatPath)
+    print("chat file")
+    
+    # load chat file
+    #print(client.get(chatPath).text)
+    a  = requests.get(url).json()
+    b = base64.b64decode(a['content'])
+    c = bytes(str(b), "utf-8").decode("unicode_escape")
+    os.system("cls")
+    print(c)
     msg = str(input(prompt))
+    if msg != "":
+        # save message to chat file
+        with open("chat.txt", "a") as myfile:
+            myfile.write(str(msg +  " \n"))
 
-    # save message to chat file
-    with open("chat.txt", "a") as myfile:
-        myfile.write(msg)
+        #commit message to git
+        
 
-    #commit message to git
-    os.system("git pull")
-    os.system("git commit *")
-    os.system("git push")
+        os.system("git pull ")
+        os.system("git commit * -m 'msg' ")
+        os.system("git push")
 
-
+    else:
+        os.system("git pull")
